03d0b8dc5e6e8d96942a6989f0f1b3042669b2dc
Manali Ghare <manalighare@Amissas-iMac.local>
2019-06-25 15:11:09 -0400

    first commit

---
+++ QRReader/ViewController.swift
@@ -0,0 +1,75 @@
+//
+//  ViewController.swift
+//  QRReader
+//
+//  Created by Manali Ghare on 6/25/19.
+//  Copyright © 2019 Manali Ghare. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController,AVCaptureMetadataOutputObjectsDelegate {
+
+    @IBOutlet weak var square: UIImageView!
+    var video = AVCaptureVideoPreviewLayer()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+        let session = AVCaptureSession()
+        
+        guard let captureDevice = AVCaptureDevice.default(for: AVMediaType.video) else {
+            print("Failed to get the camera device")
+            return
+            
+        }
+        do{
+            let input = try AVCaptureDeviceInput(device: captureDevice)
+            session.addInput(input)
+        }
+        catch{
+            print("ERROR")
+        }
+        
+        let output = AVCaptureMetadataOutput()
+        session.addOutput(output)
+        
+        output.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
+        output.metadataObjectTypes = [AVMetadataObject.ObjectType.qr]
+        
+        video = AVCaptureVideoPreviewLayer(session: session)
+        video.frame = view.layer.bounds
+        view.layer.addSublayer(video)
+        
+        self.view.bringSubviewToFront(square)
+        
+        session.startRunning()
+    }
+    
+    func metadataOutput(_ output: AVCaptureMetadataOutput, didOutput metadataObjects: [AVMetadataObject], from connection: AVCaptureConnection) {
+         if metadataObjects != nil && metadataObjects.count != 0
+         {
+            if let object = metadataObjects[0] as? AVMetadataMachineReadableCodeObject
+            {
+                if object.type == AVMetadataObject.ObjectType.qr
+                {
+                    let alert = UIAlertController(title: "QR Code", message: object.stringValue, preferredStyle: .alert)
+                    alert.addAction(UIAlertAction(title: "Retake", style: .default, handler: nil))
+                    alert.addAction(UIAlertAction(title: "Copy", style: .default, handler: { (nil) in
+                        UIPasteboard.general.string = object.stringValue
+                    }))
+                    
+                    present(alert,animated: true,completion: nil)
+                }
+            }
+        }
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    
+
+}
